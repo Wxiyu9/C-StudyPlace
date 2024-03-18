613de688bf7ceefb644c638bdb059fe10626d4d4
Wxiyu9 <w3161389382@outlook.com>
2024-03-18 17:56:35 +0800

    homework_03_07

---
+++ homework_03_07/homework_03_07/H2.c
@@ -0,0 +1,24 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <assert.h>
+////Ä£ÄâÊµÏÖstrcpy
+//char* my_strcpy(char* dest, const char* src)
+//{
+//	assert(dest != NULL);
+//	assert(src != NULL);
+//	char* ret = dest;
+//	while ((*dest++ = *src++))
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	char arr1[30];
+//	char arr2[] = "adfasdff";
+//	my_strcpy(arr1, arr2);
+//	int sz = sizeof(arr1) / sizeof(arr1[0]);
+//	printf("%s", arr1);
+//	return 0;
+//}
---
+++ homework_03_07/homework_03_07/H3.c
@@ -0,0 +1,25 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+////Ä£ÄâÊµÏÖstrcat
+//#include <stdio.h>
+//char* my_strcat(char* dest, const char* src)
+//{
+//	char* ret = dest;
+//	while (*dest != '\0')
+//	{
+//		dest++;
+//	}
+//	while ((*dest++ = *src++))
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//
+//int main()
+//{
+//	char s1[30] = "adsf";
+//	char s2[30] = "bbbb";
+//	my_strcat(s1, s2);
+//	printf("%s", s1);
+//	return 0;
+//}
